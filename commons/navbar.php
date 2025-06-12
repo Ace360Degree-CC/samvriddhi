e19e621413a8d5ec3f2fbb65e993b7d5c4d4a373
Hammed <customercare@a360pl.com>
2025-06-12 11:41:47 +0530

    header links

---
+++ commons/navbar.php
@@ -0,0 +1,26 @@
+<!-- commons/navbar.php -->
+<nav class="navbar">
+  <div class="navbar-container">
+    <div class="navbar-logo">
+      <img src="assets/images/Navbar/Samvriddhi-logo.png" alt="Samvriddhi Logo">
+    </div>
+
+    <div class="navbar-toggle" id="mobile-menu">
+      <i class="fas fa-bars"></i>
+    </div>
+
+    <ul class="navbar-menu" id="navbar-menu">
+      <li><a href="about.php">About</a></li>
+      <li><a href="#">Become a CSP</a></li>
+      <li><a href="#">Services</a></li>
+      <li><a href="#">Partners</a></li>
+      <li><a href="#">News & Media</a></li>
+      <li><a href="#">Careers</a></li>
+    </ul>
+
+    <div class="navbar-buttons">
+      <a href="#" class="btn solid">CSP Login</a>
+      <a href="#" class="btn outline">Raise a complaint</a>
+    </div>
+  </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include("commons/headerlinks.php"); ?>
+</head>
+<body>
+
+  <?php include("commons/navbar.php"); ?>
+
+  <div class="container mt-5">
+    <h1>Home Page</h1>
+    <p>This is your custom Bootstrap-free responsive site.</p>
+  </div>
+
+  <script src="js/script.js"></script>
+</body>
+</html>
